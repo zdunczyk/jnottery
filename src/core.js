c9554c9d48ba778c205cac0e38a3fc94000e6abf
zdunczyk <tomasz@zdunczyk.org>
2014-04-25 22:56:45 +0200

    simplified core with some getter/setter functions

--- src/core.js
+++ src/core.js
@@ -8,39 +8,54 @@
 
     var TT_ELEMENT = 0;
    
-    var merged_notes = {},
-        pending_notes = {};
+    var pending_notes = {};
         
     function Note(type, content, params) {
         $.extend(this, {
             type: type,
             content: content,
-            params: params
+            params: (typeof params === 'undefined' ? [] : params)
         });
     }
-
-    function getUniqueId() {
-        return Array.apply(null, Array(8)).map(function() {
+    
+    function getUniqueId(n) {
+        return Array.apply(null, Array(n)).map(function() {
             return (Math.random() * 16 | 0).toString(16);
         }).join('');
     }
 
-    function addElementNote(element, content, params, merged) {
-        var unique_id = element.data('ttid'),
-            notes = merged ? merged_notes : pending_notes;
+    function getElementId(element) {
+        return element.data('ttid');
+    }
+
+    function addElementNote(element, content, params) {
+        var unique_id = getElementId(element),
+            note_id = getUniqueId(8);
         
         if(typeof unique_id === 'undefined') {
-            unique_id = getUniqueId();
+            unique_id = getUniqueId(8);
             element.data('ttid', unique_id);
             element.addClass('ttid-' + unique_id);
         }
 
-        if($.type(notes[unique_id]) !== 'array')
-            notes[unique_id] = [];
+        if($.type(pending_notes[unique_id]) !== 'object')
+            pending_notes[unique_id] = {};
        
-        notes[unique_id].push(new Note(TT_ELEMENT, content, params));
+        pending_notes[unique_id][note_id] = new Note(TT_ELEMENT, content, params);
         
-        return unique_id;
+        return note_id;
+    }
+
+    function hasElementNote(element, note_id) {
+        var unique_id = getElementId(element);
+    
+        if(typeof pending_notes[unique_id] !== 'undefined' 
+            && typeof pending_notes[unique_id][note_id] !== 'undefined') {
+            
+            return { element_id: unique_id, note_id: note_id }; 
+        }
+
+        return null;
     }
     
     tt.core = $.extend(tt.core || {}, {
@@ -48,9 +63,36 @@
         
         // main Note object
         note: Note,
-        
+       
+        deleteElementNote: function(element, note_id) {
+            var idx = hasElementNote(element, note_id);
+            
+            if($.type(idx) === 'object')
+                delete pending_notes[idx.element_id][idx.note_id];
+        },
         addElementNote: function(element, content, params) {
-            return addElementNote(element, content, params, false);
+            return addElementNote(element, content, params);
+        },
+        editElementNote: function(element, note_id, content, params) {
+            var idx = hasElementNote(element, note_id),
+                note;
+        
+            if($.type(idx) === 'object') {
+                note = pending_notes[idx.element_id][idx.note_id];
+                if(typeof content !== 'undefined')
+                    note.content = content;
+
+                if(typeof params !== 'undefined')
+                    note.params = params;
+            }
+        },
+        getElementNotes: function(element) {
+            var unique_id = getElementId(element);
+            
+            if($.type(pending_notes[unique_id]) === 'object')
+                return pending_notes[unique_id];
+            
+            return {}; 
         },
         init: function(options) {
             var hash_decoded,
@@ -86,8 +128,7 @@
                         addElementNote(
                             elements[note.selector], 
                             note.content, 
-                            note.params, 
-                            true
+                            note.params 
                         );
                     }
                 }
@@ -99,29 +140,20 @@
                 notes = [];
             
             if(!$.isEmptyObject(pending_notes)) {
-                // merge pending array
                 for(var unique_id in pending_notes) {
-                    if(typeof merged_notes[unique_id] === 'undefined') 
-                        merged_notes[unique_id] = [];
-                   
-                    merged_notes[unique_id].push.apply(
-                        merged_notes[unique_id], 
-                        pending_notes[unique_id]
-                    );
-                } 
-                
-                for(var unique_id in merged_notes) {
-                    if(merged_notes.hasOwnProperty(unique_id)) {
+                    if(pending_notes.hasOwnProperty(unique_id)) {
                         current_selector = selectors.push($('.ttid-' + unique_id).xJQ()) - 1;
                         
-                        for(var n = 0; n < merged_notes[unique_id].length; n++) {
-                            var note = merged_notes[unique_id][n];
-                            notes.push({
-                                type: note.type,
-                                selector: current_selector,
-                                content: note.content,
-                                params: note.params
-                            });
+                        for(var note_id in pending_notes[unique_id]) {
+                            if(pending_notes[unique_id].hasOwnProperty(note_id)) {
+                                var note = pending_notes[unique_id][note_id];
+                                notes.push({
+                                    type: note.type,
+                                    selector: current_selector,
+                                    content: note.content,
+                                    params: note.params
+                                });
+                            }
                         }
                     }
                 }
