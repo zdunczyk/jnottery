8c0e3605bb1df29d55ecd8446abe1d54b5b99f11
zdunczyk <tomasz@zdunczyk.org>
2014-05-02 00:19:34 +0200

    [feat] core rebuilded, added support for selection nots

--- src/core.js
+++ src/core.js
@@ -3,106 +3,56 @@
 
 (function(tt, $) {
     var str = rayson.type.str,
-        raw = rayson.type.raw,
-        int8 = rayson.type.int8;
+        raw = rayson.type.raw;
 
-    var TT_ELEMENT = 0;
-   
-    var pending_notes = {};
+    var rootOrder = [ 'version', 'selectors', 'element_notes', 'selection_notes' ];
 
-    var rayson_keymap = {
-        root: [ 'version', 'selectors', 'notes' ],
-        notes: [ 'type', 'selector', 'content', 'params' ]
-    };
-        
-    function Note(type, content, params) {
-        $.extend(this, {
-            type: type,
-            content: content,
-            params: (typeof params === 'undefined' ? [] : params)
-        });
-    }
-    
-    function getUniqueId(n) {
-        return Array.apply(null, Array(n)).map(function() {
-            return (Math.random() * 16 | 0).toString(16);
-        }).join('');
-    }
-
-    function getElementId(element) {
-        return element.data('ttid');
-    }
-
-    function addElementNote(element, content, params) {
-        var unique_id = getElementId(element),
-            note_id = getUniqueId(8);
-        
-        if(typeof unique_id === 'undefined') {
-            unique_id = getUniqueId(8);
-            element.data('ttid', unique_id);
-            element.addClass('ttid-' + unique_id);
-        }
-
-        if($.type(pending_notes[unique_id]) !== 'object')
-            pending_notes[unique_id] = {};
-       
-        pending_notes[unique_id][note_id] = new Note(TT_ELEMENT, content, params);
-        
-        return note_id;
-    }
-
-    function hasElementNote(element, note_id) {
-        var unique_id = getElementId(element);
-    
-        if(typeof pending_notes[unique_id] !== 'undefined' 
-            && typeof pending_notes[unique_id][note_id] !== 'undefined') {
-            
-            return { element_id: unique_id, note_id: note_id }; 
-        }
-
-        return null;
-    }
-    
     tt.core = $.extend(tt.core || {}, {
         version: '0.1.0',
         
-        // main Note object
-        note: Note,
-       
-        deleteElementNote: function(element, note_id) {
-            var idx = hasElementNote(element, note_id);
-            
-            if($.type(idx) === 'object')
-                delete pending_notes[idx.element_id][idx.note_id];
-        },
-        addElementNote: function(element, content, params) {
-            return addElementNote(element, content, params);
-        },
-        editElementNote: function(element, note_id, content, params) {
-            var idx = hasElementNote(element, note_id),
-                note;
+        // internals
+        ID_PREFIX: 'ttid',
         
-            if($.type(idx) === 'object') {
-                note = pending_notes[idx.element_id][idx.note_id];
-                if(typeof content !== 'undefined')
-                    note.content = content;
+        pendingNotes: {},
 
-                if(typeof params !== 'undefined')
-                    note.params = params;
-            }
+        getElementById: function(element_id) {
+            return $('.' + this.ID_PREFIX + '-' + element_id); 
         },
-        getElementNotes: function(element) {
-            var unique_id = getElementId(element);
+        getElementId: function(element) {
+            return element.data(this.ID_PREFIX);
+        },
+        getUniqueId: function(n) {
+            return Array.apply(null, Array(n)).map(function() {
+                return (Math.random() * 16 | 0).toString(16);
+            }).join('');
+        },
+       
+        // public
+        addNote: function(note) {
+            if(note instanceof this.Note)
+                return note.save();
             
-            if($.type(pending_notes[unique_id]) === 'object')
-                return pending_notes[unique_id];
+            return null;
+        },
+        getNote: function(element, note_id) {
+            var element_notes = this.getNotes(element);
+
+            if($.type(element_notes[note_id]) === 'object')
+                return element_notes[note_id];
+
+            return null;
+        },
+        getNotes: function(element) {
+            var unique_id = this.getElementId(element);
+            
+            if($.type(this.pendingNotes[unique_id]) === 'object')
+                return this.pendingNotes[unique_id];
             
             return {}; 
         },
         init: function(options) {
             var hash_decoded,
-                elements = [],
-                note;
+                elements = [];
            
             options = $.extend({
                 hash: window.location.hash.slice(1) 
@@ -117,68 +67,71 @@
                     // encoded selectors
                     selectors: [ raw ],
                     // encoded notes
-                    notes: [{
-                        type: int8,
-                        // index from selectors array
-                        selector: int8,
-                        content: str,
-                        params: [ str ]
-                    }]
-                }, rayson_keymap);
+                    element_notes: [ this.ElementNote.template ],
+                    selection_notes: [ this.SelectionNote.template ]
+                }, {
+                    root: rootOrder,
+                    element_notes: this.ElementNote.templateOrder,
+                    selection_notes: this.SelectionNote.templateOrder
+                });
                 
-                for(var s = 0; s < hash_decoded.selectors.length; s++) {
-                    elements.push($.xJQ(hash_decoded.selectors[s]));
-                }
+                $.each(hash_decoded.selectors, function(i, selector) {
+                    elements.push($.xJQ(selector));
+                });
                 
-                for(var n = 0; n < hash_decoded.notes.length; n++) {
-                    note = hash_decoded.notes[n];
-                    
-                    if(note.type === TT_ELEMENT) {
-                        addElementNote(
-                            elements[note.selector], 
-                            note.content, 
-                            note.params 
-                        );
-                    }
-                }
+                $.each(hash_decoded.element_notes, function(i, note) {
+                    (new tt.core.ElementNote(
+                        elements[note.selector],
+                        note.content,
+                        note.params
+                    )).save();
+                });
+                
+                $.each(hash_decoded.selection_notes, function(i, note) {
+                    (new tt.core.ElementNote(
+                        elements[note.selector],
+                        note.selection,
+                        note.content,
+                        note.params
+                    )).save();
+                });
             }
         },
         merge: function() {
             var selectors = [],
                 current_selector,
-                notes = [];
+                element_notes = [],
+                selection_notes = [];
             
-            if(!$.isEmptyObject(pending_notes)) {
-                for(var unique_id in pending_notes) {
-                    if(pending_notes.hasOwnProperty(unique_id)) {
-                        current_selector = selectors.push($('.ttid-' + unique_id).xJQ()) - 1;
-                        
-                        for(var note_id in pending_notes[unique_id]) {
-                            if(pending_notes[unique_id].hasOwnProperty(note_id)) {
-                                var note = pending_notes[unique_id][note_id];
-                                notes.push({
-                                    type: note.type,
-                                    selector: current_selector,
-                                    content: note.content,
-                                    params: note.params
-                                });
-                            }
-                        }
-                    }
-                }
+            if(!$.isEmptyObject(this.pendingNotes)) {
+                $.each(this.pendingNotes, function(element_id, notes) {
+                    current_selector = selectors.push(tt.core.getElementById(element_id).xJQ()) - 1;
+                    
+                    $.each(notes, function(note_id, note) {
+                        if(note instanceof tt.core.ElementNote)
+                            element_notes.push($.extend({}, note.values(), { selector: current_selector })); 
+                        else if(note instanceof tt.core.SelectionNote)
+                            selection_notes.push($.extend({}, note.values(), { selector: current_selector }));
+                    });
+                });
                 
                 return rayson.b64.encode(rayson.serialize({
                     version: tt.core.version,
                     selectors: selectors,
-                    notes: notes 
-                }, {
-                    version: str,
-                    selectors: raw,
-                    type: int8,
-                    selector: int8,
-                    content: str,
-                    params: str 
-                }, rayson_keymap), Base64.encode);
+                    element_notes: element_notes,
+                    selection_notes: selection_notes
+                }, $.extend({
+                        version: str,
+                        selectors: raw
+                    }, 
+                    this.valuesTypes(this.ElementNote),
+                    this.valuesTypes(this.SelectionNote)
+                ), {
+                    root: rootOrder,
+                    element_notes: this.ElementNote.templateOrder,
+                    selection_notes: this.SelectionNote.templateOrder
+
+                }), Base64.encode);
             }
 
             return false;
---
+++ src/core/note.js
@@ -0,0 +1,117 @@
+// jNottery (c) 2014, Tomasz Zduńczyk <tomasz@zdunczyk.org>
+// Released under the MIT license.
+
+(function(tt, $) {
+    var str = rayson.type.str,
+        int8 = rayson.type.int8,
+        int32 = rayson.type.int32;
+
+    function extend(parent, child) {
+        var pty = function() {};
+        pty.prototype = parent.prototype;
+        child.prototype = new pty();
+        child.prototype.constructor = child;
+        return child;
+    }
+
+    function Note(element, content, params) {
+        $.extend(this, {
+            element: element,
+            content: content,
+            params: (typeof params === 'undefined' ? [] : params),
+            id: null
+        });
+    }
+
+    Note.prototype = {
+        save: function() {
+            if(this.id === null) {
+                var unique_id = tt.core.getElementId(this.element),
+                    note_id = tt.core.getUniqueId(8);
+                
+                if(typeof unique_id === 'undefined') {
+                    unique_id = tt.core.getUniqueId(8);
+                    this.element.data(tt.core.ID_PREFIX, unique_id);
+                    this.element.addClass(tt.core.ID_PREFIX + '-' + unique_id);
+                }
+
+                if($.type(tt.core.pendingNotes[unique_id]) !== 'object')
+                    tt.core.pendingNotes[unique_id] = {};
+              
+                this.id = note_id;
+                tt.core.pendingNotes[unique_id][note_id] = this;
+            }
+
+            return this.id;
+        },
+        remove: function() {
+            var element_id = tt.core.getElementId(this.element);
+            
+            if(this.id !== null && typeof element_id !== 'undefined')
+                delete tt.core.pendingNotes[element_id][this.id];
+        },
+        values: function() {
+            return {
+                content: this.content,
+                params: this.params
+            };    
+        },
+        setContent: function(txt) {
+            this.content = txt;
+        }
+    };
+       
+    Note.template = {
+        selector: int8,
+        content: str,
+        params: [ str ]
+    };
+
+    Note.templateOrder = [ 'selector', 'content', 'params' ];
+
+    var ElementNote = extend(Note, function(element, content, params) {
+        Note.call(this, element, content, params);        
+    });
+
+    ElementNote.template = Note.template;
+    ElementNote.templateOrder = Note.templateOrder;
+
+    var SelectionNote = extend(Note, function(element, selection, content, params) {
+        Note.call(this, element, content, params); 
+        this.selection = selection;
+    });
+
+    SelectionNote.prototype.values = function() {
+        return {
+            selection: this.selection,
+            content: this.content,
+            params: this.params
+        };    
+    };
+    
+    SelectionNote.template = $.extend(Note.template, {
+        selection: [ int32 ]        
+    });            
+    
+    SelectionNote.templateOrder = Note.templateOrder.concat([ 'selection' ]);
+    
+    tt.core = $.extend(tt.core || {}, {
+        Note: Note,
+        ElementNote: ElementNote,
+        SelectionNote: SelectionNote,
+
+        valuesTypes: function(note_class) {
+            var result = {};
+            
+            note_class.template && $.each(note_class.template, function(n, val) {
+                result[n] = val;
+
+                if($.isArray(result[n])) 
+                    result[n] = result[n][0];
+            });
+            
+            return result;
+        }
+    });
+    
+})(window.tt, window.jQuery);
